300a7e1c30ac9fa0ff65fc6bc661434025be61f7
Mark Milman <mark@miyasolutions.com>
2012-05-17 23:01:10 -0400

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,31 +4,43 @@ describe "Static Pages" do
 
   describe "Home Page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'SubConTraX'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'SubConTraX')
     end
 
     it "should have the right title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Home")
+                        :text => "SubConTraX | Home")
     end
   end
 
   describe "Help Page" do
 
-    it "should have the content 'Help'" do
+    it "should have the h1 'SubConTraX Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'SubConTraX Help')
+    end
+
+   it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                        :text => "SubConTraX | Help")
     end
   end
 
   describe "About page" do
 
-    it "should have the content 'About Us'" do
+    it "should have the h1 'SubConTraX About Us'" do
       visit '/static_pages/about'
-      page.should have_content('About Us')
+      page.should have_selector('h1', :text => 'SubConTraX About Us')
+    end
+
+   it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                        :text => "SubConTraX | About Us")
     end
   end
 
