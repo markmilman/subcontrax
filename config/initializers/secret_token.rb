407292c1def576af125efaf8ebe06479970701ab
Mark Milman <mark@miyasolutions.com>
2012-05-17 21:37:25 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Subcontrax::Application.config.secret_token = '0b1c6c73d9214cf8fe936a9e71bd872e320622ac44859f075317889f835c104f1bf0a42f0819b3a548d3b78479528d40c5fb4a8b3a2b5741cc7f0f2b9f876dd6'
