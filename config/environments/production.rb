068282a809593283f0ada1b528690971fd6c4bdc
Mark Milman <mark@miyasolutions.com>
2012-05-26 19:10:26 -0400

    added IDE files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Subcontrax::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
@@ -28,7 +28,7 @@ Subcontrax::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
