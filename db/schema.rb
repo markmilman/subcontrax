8b64949e748033aa01e103434197621b5c4fc8b7
Ishay Yaari <ishay@miyasolutions.com>
2012-06-07 22:20:36 -0400

    First Test Page (Welcome)

--- db/schema.rb
+++ db/schema.rb
@@ -14,13 +14,13 @@
 ActiveRecord::Schema.define(:version => 20120603203225) do
 
   create_table "users", :force => true do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.string "password_digest"
-    t.string "remember_token"
-    t.boolean "admin", :default => false
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
+    t.string   "password_digest"
+    t.string   "remember_token"
+    t.boolean  "admin",           :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
