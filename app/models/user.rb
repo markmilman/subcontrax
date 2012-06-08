3df197b8ddac7d79769bbeb61e3bb3d8d729e3b7
Mark Milman <mark@miyasolutions.com>
2012-06-07 22:46:20 -0400

    finished chapter9 exercise

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@
 
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
+  has_secure_password
+  has_many :microposts, dependent: :destroy
 
 # name validation
   validates :name, {presence: true, length: {maximum: 255}}
